e5cc2a53599269a1af840c0841b269f9ee1a6ef9
lvinokur <lea.vinokur@gmail.com>
2018-06-05 14:24:14 +1000

    matching origin master to local master

--- run_tckgen_server.py
+++ run_tckgen_server.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 import os, glob
-from mrtrix3 import app, fsl, file, image, path, run
 
-
-
-subjectspath = '~/Atlas_project/preproc_to_template/out/subjects'
+print ('initializing')
+subjectspath = '/home/lea/Atlas_project/preproc_to_template/out/subjects'
 subjects = [ os.path.split(x)[-1] for x in glob.glob(os.path.join(subjectspath, '*'))]
 
 for subject in subjects :
@@ -13,5 +11,5 @@ for subject in subjects :
     subject_path = os.path.join(subjectspath, subject)
     fod_file = os.path.join(subject_path, 'fod.mif')
     tck_file = os.path.join(subject_path, 'tck_10m.tck')
-    print('tckgen ' + fod_file + ' ' +  tck_file + ' -cutoff 0.05 -maxlength 250 -select 10000000 -seed_dynamic ' + fod_file + ' -force')
-    #os.system('tckgen ' + fod_file + ' ' +  tck_file + ' -cutoff 0.05 -maxlength 250 -select 10000000 -seed_dynamic ' + fod_file + ' -force')
+    print('tckgen ' + fod_file + ' ' +  tck_file + ' -cutoff 0.05 -maxlength 250 -select 5000000 -seed_dynamic ' + fod_file + ' -force')
+    os.system('tckgen ' + fod_file + ' ' +  tck_file + ' -cutoff 0.05 -maxlength 250 -select 10000000 -seed_dynamic ' + fod_file + ' -force')
