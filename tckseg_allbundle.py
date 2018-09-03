e4d8c677d9d41581db04e75dc2c77dfc12af4e34
lvinokur <lea.vinokur@gmail.com>
2018-09-03 16:46:38 +1000

    added tckseg_allbundle.py

--- tckseg_allbundle.py
+++ tckseg_allbundle.py
@@ -2,14 +2,16 @@
 
 import os, glob
 
-subjectspath = '/data/lea/Atlas_project/preproc_to_template/out/subjects'
+subjectspath = '/home/lea/Atlas_project/preproc_to_template/out/subjects'
 subjects = [ os.path.split(x)[-1] for x in glob.glob(os.path.join(subjectspath, '*'))]
 
-for subject in sub_subjects :
-    for mask_i in range(72):
-        os.chdir(os.path.join(subjectspath,subject))
-        maskname = os.path.join('tractseg_output',(str(mask_i) + '_mask.mif'))
-        if not os.path.exists('tsg_bundles'):
-            os.makedirs('tsg_bundles')
-        output_name = os.path.join('tsg_bundles', (str(mask_i) + '_bundle.mif'))
-        os.system('tckgen -select 20000 -seed_image ' + maskname +  ' -mask ' + maskname +  ' fod.mif ' + output_name)
+for subject in subjects :
+	print(' Working on Subject ---> ',subject )
+	os.chdir(os.path.join(subjectspath,subject))
+	for mask_i in range(72):
+		print(' Bundle : ' , mask_i )
+		maskname = os.path.join('tractseg_output',(str(mask_i) + '_mask.mif'))
+		if not os.path.exists('tsg_bundles'):
+			os.makedirs('tsg_bundles')
+		output_name = os.path.join('tsg_bundles', (str(mask_i) + '_bundle.tck'))
+		os.system('tckgen -select 20000 -seed_image ' + maskname +  ' -mask ' + maskname +  ' fod.mif ' + output_name)
